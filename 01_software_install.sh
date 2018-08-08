7d48939eb2220102d274d98bb7549656e0628d7d
knil <cols.knil@gmail.com>
2018-08-08 19:31:58 +0200

    Add installation skype and slack from .deb

--- 01_software_install.sh
+++ 01_software_install.sh
@@ -5,8 +5,16 @@ set -o pipefail
 
 apt-get update
 apt-get upgrade -y
-apt-get install -y vim steam git snapd chromium-browser
-# bug on ubuntu 18.04 don't use snap install --classic slack, qlso scudcloud didn't work at all
+# setup skype
+wget https://repo.skype.com/latest/skypeforlinux-64.deb
+dpkg -i skypeforlinux-64.deb
+apt install -f
+# setup slack
+wget https://downloads.slack-edge.com/linux_releases/slack-desktop-3.2.1-amd64.deb
+dpkg -i slack-desktop-3.2.1-amd64.deb
+apt install -f
+apt-get --fix-broken  install -y vim steam git snapd chromium-browser 
+# bug on ubuntu 18.04 don't use snap install --classic slack
 snap install --classic vscode
-snap install --classic skype
 snap install spotify
+rm *.deb*
