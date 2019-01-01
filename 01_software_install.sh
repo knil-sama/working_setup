4b40a7e54fde8c48b1f692407a8d4a91ee0ff561
clement <clement.demonchy@jobteaser.com>
2019-01-01 14:47:46 +0100

    Fix

--- 01_software_install.sh
+++ 01_software_install.sh
@@ -5,15 +5,28 @@ set -o pipefail
 
 apt-get update
 apt-get upgrade -y
+# oh my git
+# Copy the awesome fonts to ~/.fonts
+cd /tmp
+git clone http://github.com/gabrielelana/awesome-terminal-fonts
+cd awesome-terminal-fonts
+git checkout patching-strategy
+mkdir -p ~/.fonts
+cp patched/*.ttf ~/.fonts
+
+# update the font-info cache
+sudo fc-cache -fv ~/.fonts
+git clone https://github.com/arialdomartini/oh-my-git.git ~/.oh-my-git && echo source ~/.oh-my-git/prompt.sh >> ~/.bashrc
 # setup skype
 wget https://repo.skype.com/latest/skypeforlinux-64.deb
 dpkg -i skypeforlinux-64.deb
 apt install -f
 # setup slack
+apt-get install libgnome-keyring0 gir1.2-gnomekeyring-1.0
 wget https://downloads.slack-edge.com/linux_releases/slack-desktop-3.2.1-amd64.deb
 dpkg -i slack-desktop-3.2.1-amd64.deb
 apt install -f
-apt-get --fix-broken  install -y vim steam git snapd chromium-browser 
+apt-get install --fix-broken -y steam git snapd chromium-browser 
 # bug on ubuntu 18.04 don't use snap install --classic slack
 snap install --classic vscode
 snap install spotify
--- 04_vim.sh
+++ 04_vim.sh
@@ -1,24 +1,17 @@
-" From https://realpython.com/vim-and-python-a-match-made-in-heaven/
+# From https://realpython.com/vim-and-python-a-match-made-in-heaven/
 sudo apt-get install vim
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-echo """set nocompatible              " required
-filetype off                  " required
+echo "set nocompatible
+filetype off
 
-" set the runtime path to include Vundle and initialize
+\" set the runtime path to include Vundle and initialize
 set rtp+=~/.vim/bundle/Vundle.vim
 call vundle#begin()
 
-" alternatively, pass a path where Vundle should install plugins
-"call vundle#begin('~/some/path/here')
-
-" let Vundle manage Vundle, required
+\" let Vundle manage Vundle, required
 Plugin 'gmarik/Vundle.vim'
 
-" add all your plugins here (note older versions of Vundle
-" used Bundle instead of Plugin)
+\" add all your plugins here, note older versions of Vundle used Bundle instead of Plugin, All of your Plugins must be added before the following line
 
-" ...
-
-" All of your Plugins must be added before the following line
-call vundle#end()            " required
-filetype plugin indent on    " required" """ >  ~/.vimrc
+call vundle#end()
+filetype plugin indent on" >  ~/.vimrc
