4d6a9beaf8a568f0e69d751326641feed8d42711
Clement Demonchy <clement.demonchy@jobteaser.com>
2019-02-26 11:34:48 +0100

    Update 02_container_install.sh

--- 02_container_install.sh
+++ 02_container_install.sh
@@ -4,7 +4,7 @@ set -o nounset
 set -o pipefail
 
 # setup for docker*
-apt-get install apt-transport-https ca-certificates curl software-properties-common lsb-core linux-image-generic linux-image-extra-virtual
+apt-get install -y apt-transport-https ca-certificates curl software-properties-common lsb-core linux-image-generic linux-image-extra-virtual
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 \
       --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
