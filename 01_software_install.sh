df04834f33fd5b779ad59e771b3e0a0d2f013d09
Clement Demonchy <clement.demonchy@jobteaser.com>
2019-02-26 11:21:10 +0100

    Update 01_software_install.sh

--- 01_software_install.sh
+++ 01_software_install.sh
@@ -21,7 +21,7 @@ sudo fc-cache -fv ~/.fonts
 git clone https://github.com/arialdomartini/oh-my-git.git ~/.oh-my-git && echo source ~/.oh-my-git/prompt.sh >> ~/.bashrc
 #sublime text
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-apt-get install apt-transport-https
+apt-get install -y apt-transport-https
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update
 sudo apt-get install -y sublime-text
