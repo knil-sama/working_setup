bbad61560c91b5f61f85c7f1df843f7679fed4ed
Clement Demonchy <clement.demonchy@jobteaser.com>
2019-02-26 11:07:34 +0100

    Update 01_software_install.sh

--- 01_software_install.sh
+++ 01_software_install.sh
@@ -5,6 +5,7 @@ set -o pipefail
 
 apt-get update
 apt-get upgrade -y
+apt-get install -y git
 # oh my git
 # Copy the awesome fonts to ~/.fonts
 cd /tmp
@@ -17,6 +18,12 @@ cp patched/*.ttf ~/.fonts
 # update the font-info cache
 sudo fc-cache -fv ~/.fonts
 git clone https://github.com/arialdomartini/oh-my-git.git ~/.oh-my-git && echo source ~/.oh-my-git/prompt.sh >> ~/.bashrc
+#sublime text
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+apt-get install apt-transport-https
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update
+sudo apt-get install -y sublime-text
 # setup skype
 wget https://repo.skype.com/latest/skypeforlinux-64.deb
 dpkg -i skypeforlinux-64.deb
