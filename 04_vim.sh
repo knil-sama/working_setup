7723ad6a38d056dae4001724f7b36e54825f4516
Clement Demonchy <clement.demonchy@jobteaser.com>
2019-03-04 16:16:01 +0100

    Update 04_vim.sh

--- 04_vim.sh
+++ 04_vim.sh
@@ -14,4 +14,13 @@ Plugin 'gmarik/Vundle.vim'
 \" add all your plugins here, note older versions of Vundle used Bundle instead of Plugin, All of your Plugins must be added before the following line
 
 call vundle#end()
-filetype plugin indent on" >  /home/clement/.vimrc
+filetype plugin indent on
+
+set splitbelow
+set splitright
+\"split navigations
+nnoremap <C-J> <C-W><C-J>
+nnoremap <C-K> <C-W><C-K>
+nnoremap <C-L> <C-W><C-L>
+nnoremap <C-H> <C-W><C-H>
+" >  /home/clement/.vimrc
