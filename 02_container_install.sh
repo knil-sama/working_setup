de1e87d9d47118092639552a4515edaac8fe9024
Clement Demonchy <clement.demonchy@jobteaser.com>
2019-05-27 13:43:42 +0200

    Update 02_container_install.sh

--- 02_container_install.sh
+++ 02_container_install.sh
@@ -4,21 +4,21 @@ set -o nounset
 set -o pipefail
 
 # setup for docker*
-apt-get remove docker docker-engine docker.io containerd runc
-apt-get install -y apt-transport-https ca-certificates curl software-properties-common lsb-core linux-image-generic linux-image-extra-virtual
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common lsb-core linux-image-generic linux-image-extra-virtual
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 # Next, point the package manager to the official Docker repository
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
-apt-get update
-apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 # make my home computer crash wtf, use official .deb instead  apt-get install -y virtualbox
-apt-get install -y virtualbox
-snap install kubectl --classic
-snap install helm --classic
-apt-get install -y kubectx
+sudo apt-get install -y virtualbox
+sudo snap install kubectl --classic
+sudo snap install helm --classic
+sudo apt-get install -y kubectx
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.28.2/minikube-linux-amd64 && chmod +x minikube && mv minikube /usr/local/bin/
 minikube start
 chmod 774 /home/knil/.kube/config
