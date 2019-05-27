8be09b539864b2c8a33f1add2f18a11947b8fdf6
Clement Demonchy <clement.demonchy@jobteaser.com>
2019-05-27 13:16:44 +0200

    Update 01_software_install.sh

--- 01_software_install.sh
+++ 01_software_install.sh
@@ -3,9 +3,9 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-apt-get update
-apt-get upgrade -y
-apt-get install -y git curl
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install -y git curl
 # oh my git
 # Copy the awesome fonts to ~/.fonts
 cd /tmp
@@ -21,22 +21,22 @@ sudo fc-cache -fv ~/.fonts
 git clone https://github.com/arialdomartini/oh-my-git.git ~/.oh-my-git && echo source ~/.oh-my-git/prompt.sh >> ~/.bashrc
 #sublime text
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-apt-get install -y apt-transport-https
+sudo apt-get install -y apt-transport-https
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update
 sudo apt-get install -y sublime-text
 # setup skype
 wget https://repo.skype.com/latest/skypeforlinux-64.deb
-dpkg -i skypeforlinux-64.deb || apt --fix-broken install -y
-apt install -f -y
+sudo dpkg -i skypeforlinux-64.deb || sudo apt --fix-broken install -y
+sudo apt install -f -y
 # setup slack
-apt-get install -y libgnome-keyring0 gir1.2-gnomekeyring-1.0
+sudo apt-get install -y libgnome-keyring0 gir1.2-gnomekeyring-1.0
 wget https://downloads.slack-edge.com/linux_releases/slack-desktop-3.3.7-amd64.deb
-dpkg -i slack-desktop-3.3.7-amd64.deb || apt --fix-broken install -y
-apt install -f -y
-apt-get install -y steam git snapd chromium-browser 
+sudo dpkg -i slack-desktop-3.3.7-amd64.deb || sudo apt --fix-broken install -y
+sudo apt install -f -y
+sudo apt-get install -y steam git snapd chromium-browser 
 # install java
-apt install -y openjdk-11-jdk
+sudo apt install -y openjdk-11-jdk
 # go install and aws-role stuff
 #bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
 #apt-get install -y golang bison
