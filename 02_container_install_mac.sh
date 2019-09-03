769f87cb3e4baa9b2b179b70cda39b57190697b4
Clement Demonchy <vous@exemple.com>
2019-09-03 16:38:50 +0200

    Added tools

--- 02_container_install_mac.sh
+++ 02_container_install_mac.sh
@@ -6,5 +6,8 @@ set -o pipefail
 sudo port install docker
 pip3 install docker-compose --user
 sudo port install kubectl-1.15
+sudo port select --set kubectl kubectl1.15
 sudo port install helm-2.12
 sudo port select --set helm helm2.12
+sudo port install kubectx
+brew install remind101/formulae/assume-role
