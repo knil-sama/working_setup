af111df62846a8d57be6ca7384ec8d9afb7b7d2c
Clement Demonchy <vous@exemple.com>
2019-09-03 16:08:28 +0200

    Working second script for mac

---
+++ 02_container_install_mac.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+sudo port install docker
+pip3 install docker-compose --user
+sudo port install kubectl-1.15
+sudo port install helm-2.12
+sudo port select --set helm helm2.12
