229664e3bd37369729522d1a6cd0b85cb774dbd4
Clement Demonchy <clemetn@MacBook-Pro-de-JOBT-127.local>
2019-09-03 15:38:29 +0200

    working step 1 install for mac

---
+++ 01_software_install_mac.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# setup to use touch id with sudo
+
+#sudo apt-get update
+#sudo apt-get upgrade -y
+sudo port git curl 
+# oh my git
+git clone https://github.com/arialdomartini/oh-my-git.git ~/.oh-my-git && echo source ~/.oh-my-git/prompt.sh >> ~/.profile
+
+brew cask install sublime-text
+brew cask install steam
+brew cask install slack
+brew cask install skype
+brew cask install java
+brew install go
+brew cask install spotify
+brew install python
+pip3 install awscli --upgrade --user
