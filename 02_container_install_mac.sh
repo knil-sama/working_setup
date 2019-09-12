00d8bd4aa0320a3e85cb839f36f221e093ad4b68
Clement Demonchy <clement.demonchy@jobteaser.com>
2019-09-12 23:02:15 +0200

    Update 02_container_install_mac.sh

--- 02_container_install_mac.sh
+++ 02_container_install_mac.sh
@@ -9,6 +9,11 @@ sudo port select --set kubectl kubectl1.15
 sudo port install helm-2.12
 sudo port select --set helm helm2.12
 sudo port install terraform
-sudo port install kubectx
+brew install kubectx
+# gvm install
+bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
 # ksd install 
+gvm install go1.7
+gvm use go1.7 --default
+go get github.com/mfuentesg/ksd
 brew install remind101/formulae/assume-role
