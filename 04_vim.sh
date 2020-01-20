587471e09c34e7211f554a7ccfef82bc6c2dca78
Clement Demonchy <cols.knil@gmail.com>
2020-01-20 15:23:19 +0100

    Update 04_vim.sh

--- 04_vim.sh
+++ 04_vim.sh
@@ -1,13 +1,21 @@
 #https://jdhao.github.io/2018/12/24/centos_nvim_install_use_guide_en/
 sudo apt-get install vim
+pip3 install yapf jedi pynvim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-echo -n "call plug#begin('~/.local/share/nvim/plugged')
-\" For python
-Plug 'deoplete-plugins/deoplete-jedi'
-\" this one don't work https://github.com/zchee/nvim-go
-\" For golang
+echo -n "call plug#begin('~/.vim/plugged')
+Plug 'Shougo/deoplete.nvim', { 'do': ':UpdateRemotePlugins' }
+let g:deoplete#enable_at_startup = 1
+Plug 'sbdchd/neoformat'
+
+"Python
+Plug 'davidhalter/jedi-vim'
+Plug 'zchee/deoplete-jedi'
+
+" For golang
 Plug 'fatih/vim-go', { 'do': ':GoUpdateBinaries' }
+Plug 'deoplete-plugins/deoplete-go', { 'do': 'make'}
+
 #call plug#end()" > ~/.vimrc
 # PluginInstall not working in the end for neovim
 #sudo curl -fLo /usr/local/bin/nvim --create-dirs https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
