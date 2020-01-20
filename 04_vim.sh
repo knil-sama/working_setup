a0188f144d7612ad9c2fad04667cb8c199c66d14
Clement Demonchy <cols.knil@gmail.com>
2020-01-20 15:15:51 +0100

    Update 04_vim.sh

--- 04_vim.sh
+++ 04_vim.sh
@@ -1,10 +1,25 @@
-sudo apt-get install -y neovim
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+#https://jdhao.github.io/2018/12/24/centos_nvim_install_use_guide_en/
+sudo apt-get install vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+echo -n "call plug#begin('~/.local/share/nvim/plugged')
+\" For python
+Plug 'deoplete-plugins/deoplete-jedi'
+\" this one don't work https://github.com/zchee/nvim-go
+\" For golang
+Plug 'fatih/vim-go', { 'do': ':GoUpdateBinaries' }
+#call plug#end()" > ~/.vimrc
+# PluginInstall not working in the end for neovim
+#sudo curl -fLo /usr/local/bin/nvim --create-dirs https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
+#sudo chmod 755 /usr/local/bin/nvim
+#mkdir -p ~/.config/nvim
+#curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+#    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 #curl -sL install-node.now.sh/lts | sudo bash
-echo -n "call plug#begin('~/.vim/plugged')
-\"Use release branch (Recommend)
-Plug 'neoclide/coc.nvim', {'branch': 'release'}
-\" Or build from source code by use yarn: https://yarnpkg.com
-Plug 'neoclide/coc.nvim', {'do': 'yarn install --frozen-lockfile'}" > ~/.vimrc
-#https://github.com/junegunn/vim-plug/issues/741
+#echo -n "call plug#begin('~/.local/share/nvim/plugged')
+#\" For python
+#Plug 'deoplete-plugins/deoplete-jedi'
+#\" this one don't work https://github.com/zchee/nvim-go
+#\" For golang
+#Plug 'fatih/vim-go', { 'do': ':GoUpdateBinaries' }
+#call plug#end()" > ~/.config/nvim/init.vim
