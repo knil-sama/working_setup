c01b40b2d20c5e81272dd4f8b9682810a400e0db
Clement Demonchy <cols.knil@gmail.com>
2020-01-20 13:49:39 +0100

    Update 04_vim.sh

--- 04_vim.sh
+++ 04_vim.sh
@@ -1,25 +1,10 @@
-# From https://realpython.com/vim-and-python-a-match-made-in-heaven/
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-echo "set nocompatible
-filetype off
-
-\" set the runtime path to include Vundle and initialize
-set rtp+=~/.vim/bundle/Vundle.vim
-call vundle#begin()
-
-\" let Vundle manage Vundle, required
-Plugin 'gmarik/Vundle.vim'
-
-\" add all your plugins here, note older versions of Vundle used Bundle instead of Plugin, All of your Plugins must be added before the following line
-
-call vundle#end()
-filetype plugin indent on
-
-set splitbelow
-set splitright
-\"split navigations
-nnoremap <C-J> <C-W><C-J>
-nnoremap <C-K> <C-W><C-K>
-nnoremap <C-L> <C-W><C-L>
-nnoremap <C-H> <C-W><C-H>
-" >  ~/.vimrc
+sudo apt-get install -y neovim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+#curl -sL install-node.now.sh/lts | sudo bash
+echo -n "call plug#begin('~/.vim/plugged')
+\"Use release branch (Recommend)
+Plug 'neoclide/coc.nvim', {'branch': 'release'}
+\" Or build from source code by use yarn: https://yarnpkg.com
+Plug 'neoclide/coc.nvim', {'do': 'yarn install --frozen-lockfile'}" > ~/.vimrc
+#https://github.com/junegunn/vim-plug/issues/741
