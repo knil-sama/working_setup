22ce2aee02ca8e231f850a85518c8b8b3196d413
Clement Demonchy <cols.knil@gmail.com>
2022-07-05 14:21:29 +0200

    Update 01_software_install.sh
    make the script idempotent and replace skype by zoom

--- 01_software_install.sh
+++ 01_software_install.sh
@@ -7,52 +7,65 @@ sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install -y git curl
 # oh my git
-# Copy the awesome fonts to ~/.fonts
-cd /tmp
-rm -Rf awesome-terminal-fonts
-git clone http://github.com/gabrielelana/awesome-terminal-fonts
-cd awesome-terminal-fonts
-git checkout patching-strategy
-mkdir -p ~/.fonts
-cp patched/*.ttf ~/.fonts
-
-# update the font-info cache
-sudo fc-cache -fv ~/.fonts
-git clone https://github.com/arialdomartini/oh-my-git.git ~/.oh-my-git && echo source ~/.oh-my-git/prompt.sh >> ~/.bashrc
+if ! grep -qF "oh-my-git/prompt.sh" ~/.bashrc; then
+  # Copy the awesome fonts to ~/.fonts
+  cd /tmp
+  rm -Rf awesome-terminal-fonts
+  git clone http://github.com/gabrielelana/awesome-terminal-fonts
+  cd awesome-terminal-fonts
+  git checkout patching-strategy
+  mkdir -p ~/.fonts
+  cp patched/*.ttf ~/.fonts
+  # update the font-info cache
+  sudo fc-cache -fv ~/.fonts
+  git clone https://github.com/arialdomartini/oh-my-git.git ~/.oh-my-git && echo source ~/.oh-my-git/prompt.sh >> ~/.bashrc
+fi
 #sublime text
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-sudo apt-get install -y apt-transport-https
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt-get update
-sudo apt-get install -y sublime-text
-# setup skype
-wget https://repo.skype.com/latest/skypeforlinux-64.deb
-sudo dpkg -i skypeforlinux-64.deb || sudo apt --fix-broken install -y
-sudo apt install -f -y
-# setup slack
-sudo apt-get install -y libgnome-keyring0 gir1.2-gnomekeyring-1.0
-wget https://downloads.slack-edge.com/linux_releases/slack-desktop-3.3.7-amd64.deb
-sudo dpkg -i slack-desktop-3.3.7-amd64.deb || sudo apt --fix-broken install -y
-sudo apt install -f -y
-sudo apt-get install -y npm git snapd chromium-browser 
+if ! [ -x "$(command -v subl)" ]; then
+  wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+  sudo apt-get install -y apt-transport-https
+  echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+  sudo apt-get update
+  sudo apt-get install -y sublime-text
+fi
+sudo apt-get install -y npm git snapd chromium-browser
+# install slack
+sudo snap install slack
+# install zoom
+if ! [ -x "$(command -v zoom)" ]; then
+  wget https://zoom.us/client/latest/zoom_amd64.deb
+  sudo apt install ./zoom_amd64.deb
+fi
 # install java
 sudo apt install -y openjdk-11-jdk
-# go install and aws-role stuff
-#bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
-#apt-get install -y golang bison
-#source /home/clement/.gvm/scripts/gvm
-#gvm install go1.7
-#gvm use go1.7 --default
-#go get -u github.com/remind101/assume-role
-# echo "/home/clement/go/bin/assume-role" > ~/.bashrc
 # plantuml install
-apt-get install -y plantuml
-apt-get install -y awscli
-snap install --classic vscode
-snap install spotify
+sudo apt-get install -y plantuml
+# vscode install
+sudo snap install codium --classic
+codium --install-extension DavidAnson.vscode-markdownlint
+codium --install-extension yzhang.markdown-all-in-one
+codium --install-extension redhat.vscode-yaml
+codium --install-extension GitHub.vscode-pull-request-github
+codium --install-extension janjoerke.jenkins-pipeline-linter-connector
+codium --install-extension mhutchie.git-graph
+codium --install-extension ms-azuretools.vscode-docker
+codium --install-extension ms-pyright.pyright
+codium --install-extension ms-python.python
+codium --install-extension njpwerner.autodocstring
+codium --install-extension ms-python.vscode-pylance
+codium --install-extension bungcip.better-toml
+codium --install-extension esbenp.prettier-vscode
+codium --install-extension ms-toolsai.jupyter
+codium --install-extension ms-toolsai.jupyter-keymap
+codium --install-extension ms-toolsai.jupyter-renderers
+codium --install-extension ms-vscode-remote.remote-containers
+codium --install-extension ms-vscode.makefile-tools
+codium--install-extension ryanluker.vscode-coverage-gutters
+codium --install-extension shyykoserhiy.git-autoconfig
+codium --install-extension VisualStudioExptTeam.vscodeintellicode
 # git commitizen
-sudo apt-get install -y npm 
-sudo npm install -g commitizen 
+sudo apt-get install -y npm
+sudo npm install -g commitizen
 #python
 sudo apt-get install -y python3-pip
-rm *.deb*
+rm *.deb
