bbc66f1504b96ed7fa36d6c9d7698c2718e6bf3f
Clement Demonchy <cols.knil@gmail.com>
2022-07-06 11:23:35 +0200

    Update 05_tools_running_in_docker.sh

--- 05_tools_running_in_docker.sh
+++ 05_tools_running_in_docker.sh
@@ -2,3 +2,9 @@
 set -o errexit
 set -o nounset
 set -o pipefail
+
+# https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2-docker.html
+docker pull amazon/aws-cli:latest
+if ! grep -qF "alias aws" ~/.bashrc; then
+    echo "alias aws='docker run --rm -it -v ~/.aws:/root/.aws -v $(pwd):/aws amazon/aws-cli'" > ~/.bashrc
+fi
