2a02522e2e291e685e1c1faf10ef069760038b9c
Clement Demonchy <cols.knil@gmail.com>
2022-07-06 10:55:01 +0200

    Update 02_container_install.sh
    update docker, kubernetes install and make script idempotent

--- 02_container_install.sh
+++ 02_container_install.sh
@@ -3,25 +3,36 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-# setup for docker*
-sudo apt-get remove docker docker-engine docker.io containerd runc
-sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common lsb-core linux-image-generic linux-image-extra-virtual
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-# Next, point the package manager to the official Docker repository
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
-# make my home computer crash wtf, use official .deb instead  apt-get install -y virtualbox
-sudo apt-get install -y virtualbox
-sudo snap install kubectl --classic
-sudo snap install helm --classic
-sudo rm -Rf /opt/kubectx
-sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
-sudo ln -fs /opt/kubectx/kubectx /usr/local/bin/kubectx
-sudo ln -fs /opt/kubectx/kubens /usr/local/bin/kubens
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.28.2/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
-minikube start
+if ! [ -x "$(command -v docker)" ]; then
+   # setup for docker*
+   sudo apt-get remove docker docker.io containerd runc
+   sudo apt-get install -y \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+   sudo mkdir -p /etc/apt/keyrings
+   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor --batch --yes -o /etc/apt/keyrings/docker.gpg
+   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+   # Next, point the package manager to the official Docker repository
+   echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+   sudo apt-get update
+   sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+   sudo apt-get install -y virtualbox
+fi
+if ! [ -x "$(command -v kubectl)" ]; then
+   sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+   echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+   sudo apt-get update
+   sudo apt-get install -y kubectl
+fi
+snap install kubectx --classic
+if ! [ -x "$(command -v helm)" ]; then
+   curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+   sudo apt-get install apt-transport-https --yes
+   echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+   sudo apt-get update
+   sudo apt-get install -y helm
+fi
 sudo -H pip3 install docker-compose
