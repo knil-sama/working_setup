d608675f284e306300812419c2d18978ce87e272
Clement Demonchy <cols.knil@gmail.com>
2022-07-09 16:52:41 +0200

    Update 05_tools_running_in_docker.sh
    add sam install

--- 05_tools_running_in_docker.sh
+++ 05_tools_running_in_docker.sh
@@ -6,5 +6,11 @@ set -o pipefail
 # https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2-docker.html
 docker pull amazon/aws-cli:latest
 if ! grep -qF "alias aws" ~/.bashrc; then
-    echo "alias aws='docker run --rm -it -v ~/.aws:/root/.aws -v $(pwd):/aws amazon/aws-cli'" > ~/.bashrc
+    echo "alias aws='docker run --rm -it -v ~/.aws:/root/.aws -v $(pwd):/aws amazon/aws-cli'" >> ~/.bashrc
+fi
+if ! [ -x "$(command -v sam)" ]; then
+    wget https://github.com/aws/aws-sam-cli/releases/latest/download/aws-sam-cli-linux-x86_64.zip
+    unzip aws-sam-cli-linux-x86_64.zip -d sam-installation
+    sudo ./sam-installation/install
+    rm -R ./sam-installation
 fi
