e5a868a8776883ca0009266de7ad478c930ea64f
knil-sama <cols.knil@gmail.com>
2023-07-18 10:30:09 +0200

    feat{01 & 02} Explain methodo to resolve warning gpg in apt and switch to use docker compose v2

--- 01_software_install.sh
+++ 01_software_install.sh
@@ -25,6 +25,9 @@ if ! [ -x "$(command -v subl)" ]; then
   wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
   sudo apt-get install -y apt-transport-https
   echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+  # to solve warning https://www.omgubuntu.co.uk/2022/06/fix-apt-key-deprecation-error-on-ubuntu
+  # sudo apt-key list => last two 4char second line
+  # sudo apt-key export 8A8F901A | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/sublime.gpg
   sudo apt-get update
   sudo apt-get install -y sublime-text
 fi
--- 02_container_install.sh
+++ 02_container_install.sh
@@ -23,17 +23,27 @@ if ! [ -x "$(command -v docker)" ]; then
    sudo usermod -a -G docker $USER
 fi
 if ! [ -x "$(command -v kubectl)" ]; then
-   sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-   echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-   sudo apt-get update
-   sudo apt-get install -y kubectl
+   echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+   # to solve warning https://www.omgubuntu.co.uk/2022/06/fix-apt-key-deprecation-error-on-ubuntu
+   # sudo apt-key list => last two 4char second line
+   # sudo apt-key export 8A8F901A | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/kubernetes.gpg
+   curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
+   sudo apt-get install -y kubelet kubeadm kubectl
 fi
 snap install kubectx --classic
 if ! [ -x "$(command -v helm)" ]; then
    curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
    sudo apt-get install apt-transport-https --yes
    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
-   sudo apt-get update
+   sudo apt-get update 2>&1 | sed -ne 's?^.*NO_PUBKEY ??p' | xargs -r -- sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys || true
    sudo apt-get install -y helm
 fi
-sudo -H pip3 install docker-compose
+if ! [ -x "$(command -v docker compose)" ]; then
+   # pipx install docker-compose had issue running it with pyyaml, try using v2
+   mkdir -p ~/.docker/cli-plugins
+   # download the CLI into the plugins directory
+   curl -sSL https://github.com/docker/compose/releases/download/v2.20.0/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
+   # make the CLI executable
+   chmod +x ~/.docker/cli-plugins/docker-compose
+   # works using docker compose now
+fi
