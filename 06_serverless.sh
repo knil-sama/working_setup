f617353b848d63270e8bb791345a7f0a07f2ccee
knil-sama <cols.knil@gmail.com>
2023-08-14 15:10:09 +0200

    feat(06): add serverless install

---
+++ 06_serverless.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+sudo npm install -g serverless serverless-better-credentials
+# serverless config credentials --provider aws --key ${Your access key ID} --secret ${Your secret access key} --profile serverlessUser
