d3e9ba72179f796fd2b0b9a3ee89e2c470ddc416
knil-sama <cols.knil@gmail.com>
2023-08-14 15:29:02 +0200

    feat(06): add latest change

--- 06_serverless.sh
+++ 06_serverless.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 sudo npm install -g serverless serverless-better-credentials
-# serverless config credentials --provider aws --key ${Your access key ID} --secret ${Your secret access key} --profile serverlessUser
+# serverless config credentials --provider aws --key ${Your access key ID} --secret ${Your secret access key} --profile 
