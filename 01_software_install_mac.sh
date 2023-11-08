900e0a6f005ac171bcb8ae0451acec49e49a024e
Clement Demonchy <cols.knil@gmail.com>
2023-11-08 16:36:30 +0100

    Update 01_software_install_mac.sh
    remove software no longer used

--- 01_software_install_mac.sh
+++ 01_software_install_mac.sh
@@ -7,16 +7,9 @@ set -o pipefail
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install git curl
-# oh my git
-git clone https://github.com/arialdomartini/oh-my-git.git ~/.oh-my-git && echo source ~/.oh-my-git/prompt.sh >> ~/.profile
-
 brew cask install sublime-text
 brew cask install steam
 brew cask install slack
-brew cask install skype
-brew cask install java
-brew install go
-brew cask install spotify
 brew install python
 pip3 install awscli --upgrade --user
 sudo port install jq
