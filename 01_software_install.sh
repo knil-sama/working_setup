66113dc8d1b6c52ee1f8e96af68c4998ac05b430
knil-sama <cols.knil@gmail.com>
2026-02-21 09:20:51 +0100

    feat(): Add Bruno used for testing

--- 01_software_install.sh
+++ 01_software_install.sh
@@ -88,6 +88,24 @@ if ! [ -d "/usr/local/lib/node_modules" ]; then
   sudo chown -R $USER /usr/local/lib/node_modules
   echo "reload terminal to continue"
 fi
+if ! [ -x "$(command -v bruno)" ]; then
+  # Bruno install
+  # Create keyrings directory 
+  sudo mkdir -p /etc/apt/keyrings
+  # Update and install GPG and curl 
+  sudo apt update && sudo apt install gpg curl 
+  # List existing keys (optional) 
+  sudo gpg --list-keys 
+  # Add the Bruno repository key 
+  curl -fsSL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x9FA6017ECABE0266" | gpg --dearmor | sudo tee /etc/apt/keyrings/bruno.gpg > /dev/null 
+  # Set permissions for the GPG key file 
+  sudo chmod 644 /etc/apt/keyrings/bruno.gpg 
+  # Add the Bruno repository 
+  echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/bruno.gpg] http://debian.usebruno.com/ bruno stable" | sudo tee /etc/apt/sources.list.d/bruno.list 
+  # Update and install Bruno 
+  sudo apt-get update
+  sudo apt-get install bruno -y
+fi
 # need to be run outside of script nvm install node
 # need to be rn outside of script nvm use node
 if ! [ -x "$(command -v devcontainers)" ]; then
